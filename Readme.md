149b43e5203b116461dd763f298bde4a1751922b
Болотов Константин Сергеевич <pl8384@pl1.edu>
2022-04-23 13:50:31 +0500

    save

---
+++ Readme.md
@@ -0,0 +1,8 @@
+go mod init "..."
+go get "..."
+go run main.go || go run .
+go build main.go || go build .
+go mod download
+
+github.com/gin-gonic/gin
+github.com/go-pg/pg/v10
--- main.go
+++ main.go
@@ -1,66 +1,130 @@
 package main
 
 import (
-	"database/sql"
-
-	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
-	_ "github.com/go-sql-driver/mysql"
+	"github.com/go-pg/pg/v10"
 )
 
-type list struct {
-	id   int    `json:"id"`
-	info string `json:"info"`
+type Note struct {
+	tableName struct{} `pg:"notes"`
+	Id        int64    `json:"id" pg:"id,pk"`
+	CreatedAt string   `json:"created_at" pg:"created_at"`
+	Title     string   `json:"title" pg:"title"`
+	Info      string   `json:"info" pg:"info"`
 }
 
-func api(c *gin.Context) {
-	db, err := sql.Open("mysql", "root:root@/test")
+func SelectNotes() []Note {
+	var notes []Note
+	db := pgDataBase()
+
+	err := db.Model(&notes).Select()
 
 	if err != nil {
 		panic(err)
 	}
 
-	defer db.Close()
+	db.Close()
 
-	db.Exec("insert into test.list (info) value (?)", "POPA")
+	return notes
+}
+
+func InsertNote(note Note) Note {
+	db := pgDataBase()
+
+	_, err := db.Model(&note).Insert()
+
+	if err != nil {
+		panic(err)
+	}
+
+	db.Close()
+
+	return note
+}
+
+func SelectNote(id int64) Note {
+	var note Note
+	db := pgDataBase()
+
+	err := db.Model(&note).Where("id = ?", id).Select()
+
+	if err != nil {
+		panic(err)
+	}
+
+	db.Close()
+
+	return note
+
+}
+
+func pgDataBase() *pg.DB {
+	address := fmt.Sprintf("%s:%s", "localhost", "5432")
+	options := &pg.Options{
+		Addr:     address,
+		User:     "postgres",
+		Password: "postgres",
+		Database: "notice",
+		PoolSize: 50,
+	}
+
+	con := pg.Connect(options)
+
+	if con == nil {
+		log.Fatal("Нет подключения к БД")
+	}
+	return con
+}
+
+func Api(c *gin.Context) {
+	db := pgDataBase()
+
+	db.Close()
 
 	c.JSON(200, gin.H{
-		"message": "",
+		"api": "notice",
 	})
 }
-func all(c *gin.Context) {
-	db, err := sql.Open("mysql", "root:root@tcp(0.0.0.0:3306)/test")
 
+func GetNotes(c *gin.Context) {
+	c.IndentedJSON(http.StatusOK, SelectNotes())
+}
+
+func AddNote(c *gin.Context) {
+	var note Note
+
+	err := c.BindJSON(&note)
 	if err != nil {
-		panic(err)
+		return
 	}
 
-	defer db.Close()
+	note.CreatedAt = "2022-04-23T00:00:00"
 
-	rows, err := db.Query("select * from test.list")
+	c.IndentedJSON(http.StatusOK, InsertNote(note))
+}
 
-	if err != nil {
-		panic(err)
-	}
+func GetNote(c *gin.Context) {
+	id, _ := strconv.ParseInt(c.Param("id"), 10, 64)
 
-	defer rows.Close()
-
-	lists := []list{}
-
-	for rows.Next() {
-		l := list{}
-		rows.Scan(&l.id, &l.info)
-		lists = append(lists, l)
-	}
-	b, _ := json.Marshal(lists)
-
-	c.JSON(200, string(b))
+	c.IndentedJSON(http.StatusOK, SelectNote(id))
 }
 
 func main() {
 	r := gin.Default()
-	r.GET("/api", api)
-	r.GET("/all", all)
+	rApi := r.Group("/api")
+	{
+		r.GET("/notes", GetNotes)
+		rNote := rApi.Group("/note")
+		{
+			rNote.POST("/add", AddNote)
+			rNote.GET("/:id", GetNote)
+		}
+	}
+
 	r.Run("0.0.0.0:9090")
 }
